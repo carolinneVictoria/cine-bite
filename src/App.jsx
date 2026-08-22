0e04695796b58aa62c8efd9741f6ffcf3a3b9054
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-08-22 09:05:53 -0300

    refactor: restructure App component to use Background wrapper

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
 import './App.css'
+import Background from './components/Background'
 import Header from './components/Header'
 import MainSection from './components/MainSection'
 
 function App() {
 
   return (
-    <div className="bg-(--bg) w-full min-h-screen">
-      <div className="absolute top-[-286px] left-[282px] w-[400px] h-[400px]
-                  rounded-full bg-[#7B7B7F]/90 blur-[120px]" />
-
-      <div className="absolute bottom-0 top-[446px] left-[1253px] w-[451px] h-[451px]
-                  rounded-full bg-[#8CC5CF]/100 blur-[130px]" />
-      <div className="relative z-10">
-        
-        <Header />
-        <MainSection />
-      </div>
-    </div>
-  )
+        <>
+        <Background>
+          <Header />
+          <MainSection />
+        </Background>
+        </>
+  )    
 }
 
 export default App
