7cefaf9b0282f2f5b1b3ff9e171233cdcaaca349
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-08-22 09:05:59 -0300

    feat: add Background component for layout structure

---
+++ src/components/Background/index.jsx
@@ -0,0 +1,17 @@
+function Background( {children} ) {
+    return(
+        <>
+            <div className="bg-(--bg) w-full min-h-screen">
+                <div className="absolute top-[-286px] left-[282px] w-[400px] h-[400px]
+                            rounded-full bg-[#7B7B7F]/90 blur-[120px]" />
+
+                <div className="absolute bottom-0 top-[446px] left-[1253px] w-[451px] h-[451px]
+                            rounded-full bg-[#8CC5CF]/100 blur-[130px]" />
+                <div className="relative z-10"></div>
+                            {children}
+                </div>
+        </>
+    )
+}
+
+export default Background;
