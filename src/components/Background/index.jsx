94827b350186782c125f7a78b39739b0476380d6
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-08-22 09:41:37 -0300

    feat: add float-light animation to Background component divs

--- src/components/Background/index.jsx
+++ src/components/Background/index.jsx
@@ -1,14 +1,28 @@
 function Background( {children} ) {
     return(
         <>
-            <div className="absolute top-[-286px] left-[282px] w-[400px] h-[400px]
-                        rounded-full bg-[#7B7B7F]/90 blur-[120px]" />
+            <div className="absolute 
+                            top-[-286px] left-[282px] 
+                            w-[400px] h-[400px]
+                            rounded-full bg-[#7B7B7F]/90 
+                            blur-[120px] 
+                            animate-float-light"
+            />
 
-            <div className="absolute bottom-0 top-[446px] left-[1253px] w-[451px] h-[451px]
-                        rounded-full bg-[#8CC5CF]/100 blur-[130px]" />
+            <div className="absolute 
+                            bottom-0 top-[446px] left-[1253px] 
+                            w-[451px] h-[451px]
+                            rounded-full bg-[#8CC5CF]/100 
+                            blur-[130px] animate-float-light" 
+            />
             
-            <div className="absolute bottom-0 top-[1104px] left-[-87px] w-[451px] h-[451px]
-                        rounded-full bg-[#7B7B7F]/100 blur-[130px]" />
+            <div className="absolute 
+                            bottom-0 top-[1104px] left-[-87px] 
+                            w-[451px] h-[451px]
+                            rounded-full bg-[#7B7B7F]/100
+                            blur-[130px] animate-float-light 
+                            [animation-duration:18s]" 
+            />
             {children}
         </>
     )
