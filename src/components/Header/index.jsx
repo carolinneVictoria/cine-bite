45bab324ee96f624031883e7c58a0faf5753d489
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-08-23 18:28:48 -0300

    feat: update navigation links in Header component to use correct href attributes

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,10 +6,10 @@ function Header(){
             </h1>
             <nav >
                 <ul className="flex gap-17 mr-8 cursor-pointer">
-                    <li><a className="" href="#" />sobre</li>
-                    <li><a className="" href="#" />receitas</li>
-                    <li><a className="" href="#" />depoimentos</li>
-                    <li><a className="" href="#" />contato</li>
+                    <li><a className="" href="#sobre">sobre</a></li>
+                    <li><a className="" href="#receitas">receitas</a></li>
+                    <li><a className="" href="#depoimentos">depoimentos</a></li>
+                    <li><a className="" href="#contato">contato</a></li>
                 </ul>
             </nav>
         </div>
