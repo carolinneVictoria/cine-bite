a0c2275834a5ab9d472dba8673f5ed2dccae1b0a
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-08-28 21:22:03 -0300

    feat: create HomePage component with sections for Main, About, Recipe, and Testimonials

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,16 @@
+import AboutSection from "../../components/AboutSection";
+import MainSection from "../../components/MainSection";
+import RecipeSection from "../../components/RecipeSection";
+import TestimonialsSection from "../../components/TestimonialsSection";
+
+function HomePage() {
+    return (
+        <>
+            <MainSection />
+            <AboutSection />
+            <RecipeSection />
+            <TestimonialsSection />
+        </>
+    )
+}
+export default HomePage;
