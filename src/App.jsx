2c50a8438cd17e120e0ccfd89fd9d4b9189b0c36
˜carolinneVictoria˜ <carolinevictoria1224@gmail.com>
2026-08-28 23:35:50 -0300

    feat: add NotFoundPage route to handle unmatched paths

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Layout from './components/Layout'
 import ScrollToTop from './components/ScrollToTop'
 import HomePage from './pages/HomePage'
 import RecipePage from './pages/RecipePage'
+import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Route element={<Layout />} >
           <Route path="/" element={<HomePage />} />
           <Route path="/receita/:id" element={<RecipePage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </>
